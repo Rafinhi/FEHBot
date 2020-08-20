5d2303a064dc3031125ccf987fc4e37e02245fe7
Rafinhi <48394498+Rafinhi@users.noreply.github.com>
2020-08-21 01:08:20 +0200

    First version of Bot

---
+++ FehBuddy.py
@@ -0,0 +1,39 @@
+# FehBuddy.py
+import os
+
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+#GUID = os.getenv('DISCORD_GUILD')
+
+#client = discord.Client()
+bot = commands.Bot(command_prefix='FEH')
+
+
+@bot.event
+async def on_ready():
+    #guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    #print(
+        #f'{bot.user.name} is connected to the following guild:\n'
+        #f'{bot.guilds[0].name}(id: {bot.guilds[0].id})'
+    #)
+    
+    guilds = '\n - '.join([guild.name + " ID: " + str(guild.id) for guild in bot.guilds])
+    print(f'{bot.user.name} is connected to the following guilds:\n - {guilds}')
+    members = '\n - '.join([member.name for member in bot.guilds[0].members])
+    print(f'Guild Members:\n - {members}')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content == 'FEH':
+        response = 'Fire Emblem Heroes is the best Mobile Gacha Game ever created by lord Kaga sent from the heavens'
+        await message.channel.send(response)
+
+
+bot.run(TOKEN)
